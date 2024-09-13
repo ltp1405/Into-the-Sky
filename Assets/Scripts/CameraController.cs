32d81336d452be982f0ee13e5c3c8c4c0075bedb
vokhang1412 <93325449+vokhang1412@users.noreply.github.com>
2024-09-14 00:37:09 +0700

    Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] float rotationSpeed = 2f;
+    [SerializeField] float distance = 5;
+    [SerializeField] float minVerticalAngle = -45;
+    [SerializeField] float maxVerticalAngle = 45;
+    [SerializeField] Vector2 framingOffset;
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY;
+    float invertXValue;
+    float invertYValue;
+    float rotationX;
+    float rotationY;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        invertXValue = (invertX) ? -1 : 1;
+        invertYValue = (invertY) ? -1 : 1;
+        rotationX += Input.GetAxis("Mouse Y") * invertYValue * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+
+        rotationY += Input.GetAxis("Mouse X") * invertXValue * rotationSpeed;
+
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        var focusPosition = followTarget.position + new Vector3(framingOffset.x, framingOffset.y);
+
+        transform.position = focusPosition - targetRotation * new Vector3(0, 0, distance);
+        transform.rotation= targetRotation;
+    }
+}
