688672de226a56fc7a4ccbb7a2781d52ad865606
Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-09-19 21:38:15 +0700

    add main menu into scene

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,9 +5,57 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject player;
+
+    public GameObject mainMenu;
+
+    BasicBehaviour basicBehaviour;
+    MoveBehaviour moveBehaviour;
+    AimBehaviourBasic aimBehaviourBasic;
+    FlyBehaviour flyBehaviour;
+    ThirdPersonOrbitCamBasic thirdPersonOrbitCamBasic;
+
+    void Awake()
+    {
+        basicBehaviour = player.GetComponent<BasicBehaviour>();
+        moveBehaviour = player.GetComponent<MoveBehaviour>();
+        aimBehaviourBasic = player.GetComponent<AimBehaviourBasic>();
+        flyBehaviour = player.GetComponent<FlyBehaviour>();
+        thirdPersonOrbitCamBasic = player.GetComponentInChildren<ThirdPersonOrbitCamBasic>();
+    }
+
+    public void EnablePlayerBehaviours()
+    {
+        basicBehaviour.enabled = true;
+        moveBehaviour.enabled = true;
+        aimBehaviourBasic.enabled = true;
+        flyBehaviour.enabled = true;
+        thirdPersonOrbitCamBasic.enabled = true;
+    }
+
+    void DisablePlayerBehaviours()
+    {
+        basicBehaviour.enabled = false;
+        moveBehaviour.enabled = false;
+        aimBehaviourBasic.enabled = false;
+        flyBehaviour.enabled = false;
+        thirdPersonOrbitCamBasic.enabled = false;
+    }
+
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        DisablePlayerBehaviours();
+    }
+
     public void PlayGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Playground++");
+        mainMenu.SetActive(false);
+        EnablePlayerBehaviours();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        // UnityEngine.SceneManagement.SceneManager.LoadScene("Playground++");
     }
 
     public void QuitGame()
